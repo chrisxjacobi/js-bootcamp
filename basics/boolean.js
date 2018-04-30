80a277537ee10e84c53db7da9a430100b399069f
chrisxjacobi <chrisxjacobi@gmail.com>
2018-04-29 20:08:59 -0500

    es6 conditionals and operators

---
+++ basics/boolean.js
@@ -0,0 +1,38 @@
+// === is the equality operator
+// !== is the not equal operator
+// < less than operator
+// > greater than operator
+// <= less than or equal to operator
+// >= greater than or equal to operator
+
+let temp = 180
+
+if (temp <= 32) {
+    console.log('It is freezing outside!');
+}
+
+if (temp >= 110) {
+    console.log('It is way too hot outside');
+    console.log('testing123');
+}
+
+// challenge area
+
+// create age to 32
+// calcualte is child (if 7 or under)
+// calculate is senior (if 65 or older)
+// print both
+
+let age = 3
+
+// if 7 or under, print about child pricing
+// if 65 or older, print about senior discount
+
+if (age <= 7) {
+    console.log('you get child ticket price');
+}
+
+if (age >= 65) {
+    console.log('you get senior discount');
+    
+}
---
+++ basics/temp-conversion.js
@@ -0,0 +1,13 @@
+let fahrenheit = 50  // 0c, 273.15k
+let celsius = (fahrenheit - 32) * (5/9);
+let kelvin = (fahrenheit + 459.67) * (5/9);
+
+console.log('Celsius: ' + celsius);
+console.log('Kelvin: ' + kelvin);
+
+
+
+
+
+// calc celsius and store, print
+// calc kelvin and store, print
