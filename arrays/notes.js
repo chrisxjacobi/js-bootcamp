130458a472451db34c7b778405bfeaf888d2b628
chrisxjacobi <chrisxjacobi@gmail.com>
2018-05-07 20:59:33 -0500

    added a few array methods to find by title/text and by index

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,13 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habits to work on',
+    body: 'exercise'
+}, {
+    title: 'office mods',
+    body: 'get a new seat'
+}]
 
 // console.log(notes.pop())
 // notes.push('My new note')
@@ -8,22 +17,46 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 
 // notes.splice(1, 1, 'this is the new second note')
 
-notes[2] = 'this is the new note 3'
+// notes[2] = 'this is the new note 3'
 
-notes.forEach(function (item, index) {
-    console.log(index)
-    console.log(item)
-    
-})
-
-console.log(notes.length);
-console.log(notes);
+// notes.forEach(function (item, index) {
+//     console.log(index)
+//     console.log(item)
+// })
 
 // counting... 1, 
-for (let count = 2; count >= 0; count--) {
-    console.log(count);
+// for (let count = 2; count >= 0; count--) {
+//     console.log(count);
+// }
+
+// for (let count = notes.length - 1; count >= 0; count--) {
+//     console.log(notes[count]);
+// }
+
+
+const findNote = function (notes, noteTitle) {
+    return notes.find(function (note, index) {
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+    
 }
 
-for (let count = notes.length - 1; count >= 0; count--) {
-    console.log(notes[count]);
-}
+// const findNote = function (notes, noteTitle) {
+//     const indexNote = notes.findIndex(function (note, index) {
+//         return note.title.toLowerCase() === noteTitle.toLowerCase()
+//     })
+//     return notes[indexNote]
+// }
+
+const note = findNote(notes, 'Office mods')
+console.log(note);
+
+
+// console.log(notes.length);
+// console.log(notes);
+
+// const indexNote = notes.findIndex(function (note, index) {
+//     return note.title === 'Habits to work on'
+// })
+
+// console.log(indexNote);
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,24 +1,59 @@
 // array with five todos, you have X todoes, print the first and second to last items
 
-const todos = ['walk the dog', 'eat dinner', 'go to the gym', 'study JS', 'kiss bb'];
+const todos = [{
+    text: 'eat dinner',
+    completed: false
+}, {
+    text: 'walk the dog',
+    completed: false
+}, {
+    text: 'go to gym',
+    completed: true
+}, {
+    text: 'kiss bb',
+    completed: false
+}, {
+    text: 'pet sunny',
+    completed: true
+}]
+
+const deleteTodo = function (todos, text) {
+    const index = todos.findIndex(function (todo) {
+        return todo.text.toLowerCase() === text.toLowerCase()
+    })
+
+    if (index > -1) {
+        todos.splice(index, 1)
+    }
+}
+
+deleteTodo(todos, 'go to gym')
+console.log(todos);
+
+    
+    
+
+// convert array to array of obj (text, completed), create function to remove by text value
+
+
+
 
 // delete third item, add new item on end, remove first item
-todos.splice(2, 1)
-todos.push('pet sunny')
-todos.shift()
+// todos.splice(2, 1)
+// todos.push('pet sunny')
+// todos.shift()
 
-console.log(`You have ${todos.length} todos!`);
+// console.log(`You have ${todos.length} todos!`);
 
-// 1, first item, 2, seconde item, 3, third item
+// 1, first item, 2, second item, 3, third item
 
-todos.forEach(function (todo, index) {
-    let num = index + 1
-    console.log(`${index + 1}. ${todo}`);  
-})
+// todos.forEach(function (todo, index) {
+//     let num = index + 1
+//     console.log(`${index + 1}. ${todo}`);  
+// })
 
-for (let count = 0; count < todos.length; count++) {
-    let num = count + 1
-    let todo = todos[count]
-    console.log(`${num}. ${todo}`);
-    
-}
+// for (let count = 0; count < todos.length; count++) {
+//     let num = count + 1
+//     let todo = todos[count]
+//     console.log(`${num}. ${todo}`);
+// }
