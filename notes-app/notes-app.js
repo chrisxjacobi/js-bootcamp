2e57f7ccde27b8343274cd2518bbdf7ebe88e5fb
chrisxjacobi <chrisxjacobi@gmail.com>
2018-06-05 21:10:07 -0500

    basics of localStorage

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -13,6 +13,25 @@ const filters = {
     searchText: ''
 }
 
+// const user = {
+//   name: 'Chris',
+//   age: 32
+// }
+
+// const userJSON = JSON.stringify(user)
+// console.log(userJSON)
+// localStorage.setItem('user', userJSON)
+
+const userJSON = localStorage.getItem('user')
+const user = JSON.parse(userJSON)
+console.log(`${user.name} is ${user.age} years old.`);
+
+// localStorage.setItem('location', 'Austin');
+// console.log(localStorage.getItem('location'))
+// localStorage.removeItem('location')
+// localStorage.clear()
+
+
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
