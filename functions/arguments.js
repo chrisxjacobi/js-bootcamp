fb232577c1cf2dee63ace4f3dd2dcdc568db8aee
chrisxjacobi <chrisxjacobi@gmail.com>
2018-06-26 13:02:36 -0500

    adding function files, starting objects

--- functions/arguments.js
+++ functions/arguments.js
@@ -1,32 +1,62 @@
 // multiple arguments
 
+
 let add = function (a, b, c) {
-    return a + b + c
+  return a + b + c
 }
 
 let result = add(10, 1, 5)
 console.log(result);
 
-// default arguments
-let getScoreText = function (name = 'Anonymous', score = 0) {
-    return `Name: ${name} - Score: ${score}`
-
+let getScoreText = function (name = 'anon', score = 0) {
+  return `Name: ` + name + ' - Score: ' + score
 }
 
 let scoreText = getScoreText(undefined, 99)
 console.log(scoreText);
 
-// challenge area, getTip()
-// total, tipPercentage, 'A 25% tip on 40$ would be: '
 
-let getTip = function (bill, tipPercentage = .1) {
-    let percent  = tipPercentage * 100
-    let tip = bill * tipPercentage
-    let total = bill + (bill * tipPercentage);
-   // return `Bill: $${bill}, Tip: $${bill * tipPercentage}, Total: $${total}.`
-    return `A ${percent}% tip on $${bill} would be $${tip}`
 
+let getTip = function (total, tipPercent = .2) {
+  let bill = total + (total * tipPercent)
+  let tip = tipPercent * 100
+  let percentage = total * tipPercent
+
+  return `A ${tip}% tip on $${total} would be $${percentage}`
 }
 
-let totalBill = getTip(50)
-console.log(totalBill);
+let payment = getTip(75, undefined)
+console.log(payment);
+
+
+
+// let add = function (a, b, c) {
+//     return a + b + c
+// }
+//
+// let result = add(10, 1, 5)
+// console.log(result);
+//
+// // default arguments
+// let getScoreText = function (name = 'Anonymous', score = 0) {
+//     return `Name: ${name} - Score: ${score}`
+//
+// }
+//
+// let scoreText = getScoreText(undefined, 99)
+// console.log(scoreText);
+//
+// // challenge area, getTip()
+// // total, tipPercentage, 'A 25% tip on 40$ would be: '
+//
+// let getTip = function (bill, tipPercentage = .1) {
+//     let percent  = tipPercentage * 100
+//     let tip = bill * tipPercentage
+//     let total = bill + (bill * tipPercentage);
+//    // return `Bill: $${bill}, Tip: $${bill * tipPercentage}, Total: $${total}.`
+//     return `A ${percent}% tip on $${bill} would be $${tip}`
+//
+// }
+//
+// let totalBill = getTip(50)
+// console.log(totalBill);
--- functions/grade-calc.js
+++ functions/grade-calc.js
@@ -1,24 +1,47 @@
 // studentScore, totalPossibleScore...generate letter grade and percentage
 // 'You got a C (75%)!'
 
-const gradeCalc = function (studentScore, totalPossibleScore = 100) {
-    const percentage = (studentScore / totalPossibleScore) * 100
-    let letterGrade = ''
+ const gradeCalc = function (studentScore, totalPossibleScore = 100) {
+   let percent = (studentScore/totalPossibleScore) * 100
+   let letter;
 
-    if (percentage >= 90) {
-         letterGrade = 'A'
-    } else if (percentage >= 80) {
-        letterGrade = 'B'
-    } else if (percentage >= 70) {
-        letterGrade = 'C'
-    } else if (percentage >= 65) {
-        letterGrade = 'D'
-    } else {
-        letterGrade = 'F'
-    }
+   if (percent >= 90) {
+     letter = 'A'
+   } else if (percent >= 80) {
+     letter = 'B'
+   } else if (percent >= 70) {
+     letter = 'C'
+   } else if (percent >= 60){
+     letter = 'D'
+   } else {
+     letter = 'F'
+   }
 
-    return `You got a ${letterGrade}! (${percentage}%)`
-}
+   return `You got a ${letter} with a percentage of ${percent}%!`
+ }
 
-const grade = gradeCalc(94);
-console.log(grade);
+ let total = gradeCalc(120)
+ console.log(total);
+
+
+// const gradeCalc = function (studentScore, totalPossibleScore = 100) {
+//     const percentage = (studentScore / totalPossibleScore) * 100
+//     let letterGrade = ''
+//
+//     if (percentage >= 90) {
+//          letterGrade = 'A'
+//     } else if (percentage >= 80) {
+//         letterGrade = 'B'
+//     } else if (percentage >= 70) {
+//         letterGrade = 'C'
+//     } else if (percentage >= 65) {
+//         letterGrade = 'D'
+//     } else {
+//         letterGrade = 'F'
+//     }
+//
+//     return `You got a ${letterGrade}! (${percentage}%)`
+// }
+//
+// const grade = gradeCalc(94);
+// console.log(grade);
