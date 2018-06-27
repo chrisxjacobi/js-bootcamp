57fccc4d9f191245f66bf09c075b720c3275752e
chrisxjacobi <chrisxjacobi@gmail.com>
2018-06-27 12:40:25 -0500

    methods/string methods

--- objects/objects-methods.js
+++ objects/objects-methods.js
@@ -1,24 +1,49 @@
 let restaurant = {
-    name: 'Olive Garden',
-    guestCapacity: 75,
-    guestCount: 0,
-    checkAvailability: function (partySize) {
-        let seatsLeft = (this.guestCapacity - this.guestCount)        
-        return partySize <= seatsLeft;
-    },
-    seatParty: function (partySize) {
-        this.guestCount = (this.guestCount + partySize)
-    },
-    removeParty: function (partySize) {
-        this.guestCount = (this.guestCount - partySize);
+  name: 'Outback',
+  guestCapacity: 75,
+  guestCount: 0,
+  checkAvailability: function(partySize) {
+    let seatsLeft = this.guestCapacity - this.guestCount
+    if (partySize <= seatsLeft) {
+      return `We can seat a party size of ${partySize}`
+    } else {
+      return `We cannot seat such a large party of ${partySize}`
     }
-
+  },
+  seatParty: function(partySize) {
+    this.guestCount = this.guestCount + partySize
+  },
+  removeParty: function(partySize) {
+    this.guestCount = this.guestCount - partySize
+  }
 }
 
-// seatParty - party size added to guest count
-// removeParty - party size removed from guest count
-
 restaurant.seatParty(72)
-console.log(restaurant.checkAvailability(4));
+console.log(restaurant.checkAvailability(4))
 restaurant.removeParty(5)
-console.log(restaurant.checkAvailability(4));
+console.log(restaurant.checkAvailability(4))
+
+// let restaurant = {
+//     name: 'Olive Garden',
+//     guestCapacity: 75,
+//     guestCount: 0,
+//     checkAvailability: function (partySize) {
+//         let seatsLeft = (this.guestCapacity - this.guestCount)
+//         return partySize <= seatsLeft;
+//     },
+//     seatParty: function (partySize) {
+//         this.guestCount = (this.guestCount + partySize)
+//     },
+//     removeParty: function (partySize) {
+//         this.guestCount = (this.guestCount - partySize);
+//     }
+//
+// }
+//
+//  seatParty - party size added to guest count
+//  removeParty - party size removed from guest count
+//
+// restaurant.seatParty(72)
+// console.log(restaurant.checkAvailability(4));
+// restaurant.removeParty(5)
+// console.log(restaurant.checkAvailability(4));
--- objects/string-methods.js
+++ objects/string-methods.js
@@ -1,27 +1,53 @@
-let name = '    Chris Jacobi      '
-
-// length property
+let name = '    Chris Jacobi   '
 console.log(name.length);
 
-// convert to uppercase
 console.log(name.toUpperCase());
 
-// convert to lowercase
 console.log(name.toLowerCase());
 
-// includes
-let password = 'abc12323r098'
-console.log(password.includes('password'));
+let password = '394huitrge09food89ui3re'
+console.log(password.includes('food'))
 
-// trim
 console.log(name.trim());
 
-// challenge area - length more than 8, does not contain 'password'
 
-let isValidPassword = function (pw) {
-    return pw.length > 8 && !pw.includes('password')
+let isValidPassword = function (str) {
+  return str.length > 8 && !str.includes('password')
 }
 
-console.log(isValidPassword('asft'));
-console.log(isValidPassword('sdfunjf238!@#REFW'));
-console.log(isValidPassword('sdfunjf23password8!@#REFW'));
+console.log(isValidPassword('coolio'));
+console.log(isValidPassword('huinj49f8ijo34'));
+console.log(isValidPassword('password'));
+console.log(isValidPassword('jj'));
+console.log(isValidPassword('jjy8hiunj4reguijpassword7huij4reg'));
+
+
+
+
+// let name = '    Chris Jacobi      '
+//
+// // length property
+// console.log(name.length);
+//
+// // convert to uppercase
+// console.log(name.toUpperCase());
+//
+// // convert to lowercase
+// console.log(name.toLowerCase());
+//
+// // includes
+// let password = 'abc12323r098'
+// console.log(password.includes('password'));
+//
+// // trim
+// console.log(name.trim());
+//
+// // challenge area - length more than 8, does not contain 'password'
+//
+// let isValidPassword = function (pw) {
+//     return pw.length > 8 && !pw.includes('password')
+// }
+//
+// console.log(isValidPassword('asft'));
+// console.log(isValidPassword('sdfunjf238!@#REFW'));
+// console.log(isValidPassword('sdfunjf23password8!@#REFW'));
