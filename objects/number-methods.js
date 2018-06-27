a96f074f116a2adb35a40eb0ccc1e7d1bd28f1a9
chrisxjacobi <chrisxjacobi@gmail.com>
2018-06-27 16:39:17 -0500

    adding number methods info

--- objects/number-methods.js
+++ objects/number-methods.js
@@ -1,24 +1,54 @@
-let num = 893.34809023
+let num = 79234.23948
 
 console.log(num.toFixed(2));
 
-console.log(Math.round(num));
-console.log(Math.floor(num));
-console.log(Math.ceil(num));
+// console.log(Math.round(num))
+// console.log(Math.floor(num))
+// console.log(Math.ceil(num))
 
-let min = 10
-let max = 20
 
-let randomNum = Math.floor(Math.random() * (max - min)) + min
-console.log(randomNum);
+let min = 0
+let max = 1
+let random = Math.floor(Math.random() * (max - min + 1)) + min
+console.log(random);
 
-// challenge area  - fn takes in guess, random between 1 - 5, T/F
 
-let rand = Math.floor(Math.random() * 5) + 1
-//console.log(rand);
+let makeGuess = function (num) {
+  let max = 5
+  let min = 1
 
-let makeGuess = function (guess) {
-    return guess === rand
+  let rand = Math.floor(Math.random() * (max - min +1)) + min
+
+  return rand === num
+
 }
 
 console.log(makeGuess(3));
+
+
+
+
+// let num = 893.34809023
+//
+// console.log(num.toFixed(2));
+//
+// console.log(Math.round(num));
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+//
+// let min = 10
+// let max = 20
+//
+// let randomNum = Math.floor(Math.random() * (max - min)) + min
+// console.log(randomNum);
+//
+// // challenge area  - fn takes in guess, random between 1 - 5, T/F
+//
+// let rand = Math.floor(Math.random() * 5) + 1
+// //console.log(rand);
+//
+// let makeGuess = function (guess) {
+//     return guess === rand
+// }
+//
+// console.log(makeGuess(3));
