bd19392554bd1f35eec6e0474db1eb9f995ef721
chrisxjacobi <chrisxjacobi@gmail.com>
2018-07-02 16:45:38 -0500

    adding expense/income tracker with light methods

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,3 +1,70 @@
+const account = {
+  name: 'Chris J',
+  expenses: [],
+  income: [],
+  addExpense: function (description, amount) {
+    this.expenses.push({
+      description: description,
+      amount: amount
+    })
+  },
+  addIncome: function (description, amount) {
+    this.income.push({
+      description: description,
+      amount: amount
+    })
+  },
+  getAccountSummary: function () {
+    let totalExpenses = 0
+    let totalIncome = 0
+    let balance;
+
+    this.expenses.forEach(function (expense) {
+      totalExpenses = totalExpenses + expense.amount
+    })
+
+    this.income.forEach(function (income) {
+      totalIncome = totalIncome + income.amount
+    })
+
+    balance = totalIncome - totalExpenses
+
+    return `${this.name} has a balance of $${balance}. This includes $${totalExpenses} in expenses and $${totalIncome} in income.`
+  }
+}
+
+
+account.addExpense('Rent', 950)
+account.addExpense('coffee', 2)
+account.addIncome('job', 1000)
+console.log(account.getAccountSummary());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
