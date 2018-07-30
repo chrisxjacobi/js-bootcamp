eb9171d9b55412fc4f1f55ad522902e78dea8219
chrisxjacobi <chrisxjacobi@gmail.com>
2018-07-29 19:17:46 -0500

    quick dom manipluation

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,14 @@
-console.log('this is from a different file')
+// document object model
+
+// query and remove
+//const p = document.querySelector('p')
+//p.remove()
+
+// query all and remove
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function (p) {
+    p.textContent = '*****'
+    // console.log(p.textContent)
+    // p.remove()
+})
